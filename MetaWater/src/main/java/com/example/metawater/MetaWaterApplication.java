bc451421333c06fe430998943da78ce92c6d2852
seeun jeong <sesej4512@gmail.com>
2023-04-06 14:43:08 +0900

    Feat: springboot 설정

---
+++ MetaWater/src/main/java/com/example/metawater/MetaWaterApplication.java
@@ -0,0 +1,13 @@
+package com.example.metawater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaWaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaWaterApplication.class, args);
+    }
+
+}
